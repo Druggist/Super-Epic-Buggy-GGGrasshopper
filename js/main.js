bda6efb419ccd797c30d4ca5a9e06d2361b2f5f0
Druggist <m.najdora@gmail.com>
2015-11-06 23:10:08 +0100

    Victory & defeat messages

--- js/main.js
+++ js/main.js
@@ -2,19 +2,21 @@ var scene, camera, renderer, keyboard, clock;
 var cube, player, death_plane;
 
 // Setup PhysiJS
-Physijs.scripts.worker = '/js/physijs_worker.js';
-Physijs.scripts.ammo = '/js/ammo.js';
+Physijs.scripts.worker = 'js/physijs_worker.js';
+Physijs.scripts.ammo = 'ammo.js';
 
 var DEBUG = true;
 var MOVE_SPEED = 10;
-var JUMP_HEIGHT = 10;
+var JUMP_HEIGHT = 5;
 var BOUNCE_HEIGHT = 3;
-var LVL_LENGHT = 30;
+var LVL_LENGTH = 50;
 var MAX_VELOCITY = 30;
 var MIN_HEIGHT = -1;
 var PLAYER_OFFSET = 1;
-var GRAVITY = 5;
-var FOV = 95;
+var GRAVITY = 10;
+var FOV = 100;
+var VICTORY = false;
+var PAUSE = false;
 
 window.onload = init();
 
@@ -28,35 +30,37 @@ function getRandomGreenColor(){
 
 function generate(blocks) {
 	var block_height = 1;
-	var block_width = 2;
+	var block_min_width = 1;
+	var block_max_width = 3;
 	var block_depth = 3;
 	var max_distanceY = 2;
 	var min_distanceY = -2;
-	var max_distanceX = 4;
+	var max_distanceX = 8;
 	var min_distanceX = 2;
 	var max_height =5;
 	var min_height = MIN_HEIGHT;
-	var positionX = 0;
+	var positionX = 4;
 	var positionY = 0;
 	var green_color = [0x7B7922, 0xCECC15, 0xCDD704, 0xA2BC13, 0x859C27, 0x668014, 0xBEE554, 0xCDAD00, 0x8B7500, 0xAEBB51];
 	cube = [];
 
 	for (var i = 0; i < blocks; i++) {
+			var width =  getRandomInt(block_min_width, block_max_width);
 		cube[i] = new Physijs.BoxMesh(
-			new THREE.BoxGeometry( block_width, block_height, block_depth ),
-			new THREE.MeshBasicMaterial( { color: green_color[getRandomGreenColor()],  } ),
+			new THREE.BoxGeometry(width, block_height, block_depth ),
+			new THREE.MeshLambertMaterial( { color: green_color[getRandomGreenColor()],  } ),
 			0
 		);
 		cube[i].position.x= positionX;
 		cube[i].position.y= positionY;
 		if (i == 0) {
-			cube[i].scale.x = 5;
-			cube[i].position.x += -4;
+			cube[i].scale.x = 20.0/width;
+			cube[i].position.x -= 9;
 		}
 		if (i == blocks - 1) {
 			cube[i].material.color = cube[0].material.color;
-			cube[i].scale.x = 8;
-			cube[i].position.x += 7;
+			cube[i].scale.x = 20.0/width;
+			cube[i].position.x += 9;
 
 		}
 		scene.add( cube[i] );
@@ -103,7 +107,8 @@ function init() {
 		'update',
 		function() {
 			player.setAngularVelocity(new THREE.Vector3(0,0,0));
-			scene.simulate( undefined, 1 );
+			if(!PAUSE)
+				scene.simulate( undefined, 1 );
 			physics_stats.update();
 		}
 	);
@@ -128,7 +133,13 @@ scene.add( directionalLight );
 		new THREE.BoxGeometry( 1, 1, 1 ),
 		new THREE.MeshPhongMaterial( { color: 0x8AB800 } )
 	);
-	player.addEventListener( 'collision', function() {
+	player.addEventListener( 'collision', function(collidingObject) {
+			if(collidingObject == cube[LVL_LENGTH-1]){
+				$(".victory").removeClass("hidden");
+				VICTORY = true;
+				PAUSE = true;
+			}
+			
 			force = BOUNCE_HEIGHT;
 			if (this.isJumping) {
 				force += JUMP_HEIGHT;
@@ -143,27 +154,31 @@ scene.add( directionalLight );
 	scene.add( player );
 
 	// GENERATE LEVEL
-	generate(LVL_LENGHT);
+	generate(LVL_LENGTH);
 
 	requestAnimationFrame( render );
 	scene.simulate();
 }
 
 function render() {
-
 	requestAnimationFrame( render );
-	if (player.position.y < -1) {
-		player.setLinearVelocity(new THREE.Vector3(0,0,0))
-		player.position.y = PLAYER_OFFSET;
-		player.position.x = 0;
-		player.__dirtyPosition = true;
+	if (player.position.y < -2) {
+		$(".defeat").removeClass("hidden");
+		if(keyboard.pressed("r"))
+		reset(false);
+	}
+	
+	if(VICTORY && keyboard.pressed("r")){
+		reset(true);
+		PAUSE = false;
+		scene.simulate( undefined, 1 );
 	}
 
 	delta = clock.getDelta();
 	v = player.getLinearVelocity();
-	if(keyboard.pressed("A")) {
+	if(keyboard.pressed("a") || keyboard.pressed("left")) {
 		if(Math.abs(v.x) < MAX_VELOCITY) player.applyCentralImpulse(new THREE.Vector3(-MOVE_SPEED * delta,0,0));
-	} else if (keyboard.pressed("D")) {
+	} else if (keyboard.pressed("d") || keyboard.pressed("right")) {
 		if(Math.abs(v.x) < MAX_VELOCITY) player.applyCentralImpulse(new THREE.Vector3(MOVE_SPEED * delta,0,0));
 	} else {
 		if(v.x>0){
@@ -172,7 +187,7 @@ function render() {
 				player.applyCentralImpulse(new THREE.Vector3(MOVE_SPEED * delta,0,0))
 		}
 	}
-	if (keyboard.pressed("w")) {
+	if (keyboard.pressed("w") || keyboard.pressed("up") || keyboard.pressed("space")) {
 		player.isJumping = true;
 	}
 	camera.position.x = player.position.x;
@@ -182,3 +197,20 @@ function render() {
 	renderer.render( scene, camera );
 	render_stats.update();
 }
+
+function reset(FullReset) {
+	if(FullReset){
+		for(var i=0; i<LVL_LENGTH;i++)	scene.remove(cube[i]);
+		cube = null;
+		generate(LVL_LENGTH);
+	}
+	player.setLinearVelocity(new THREE.Vector3(0,0,0));
+	player.position.y = PLAYER_OFFSET;
+	player.position.x = 0;
+	player.__dirtyPosition = true;
+	$(".defeat").addClass("hidden");
+	$(".victory").addClass("hidden");
+	VICTORY = false;
+}
+
+
